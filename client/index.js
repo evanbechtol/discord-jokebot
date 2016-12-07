e84f348d9a3798c9961db606ada66843f7d40411
Evan Bechtol <evanbechtol@gmail.com>
2016-12-07 14:35:44 -0600

    Added promise support and jokes are read from file asynchronously, using promises

--- client/index.js
+++ client/index.js
@@ -1,20 +1,43 @@
+let fs = require('fs');
+const Promise = require('es6-promise').Promise;
+
 module.exports = (client, Events, _) => {
     client.Dispatcher.on(Events.GATEWAY_READY, () => {
         console.log('Connected as: ' + client.User.username);
     });
 
     client.Dispatcher.on(Events.MESSAGE_CREATE, (e) => {
-        console.log('message received');
         let data = e.message.content.split(' ');
 
         if (_.first(data) === '!joke' && data.length > 1) {
             let name = e.message.mentions[0].username;
-            let joke = name + ', if laughter is the best medicine, your face must be curing the world.';
-
-            e.message.channel.sendMessage(joke);
+            //let joke = name + ', if laughter is the best medicine, your face must be curing the world.';
+            getRandomLine().then(function (joke) {
+                e.message.channel.sendMessage(name + joke);
+            });
 
         } else if (e.message.content === '!joke' && e.message.mentions.length === 0) {
             e.message.channel.sendMessage(('You must mention someone to make a joke about!'));
         }
     });
+
+    function getRandomLine() {
+        let filename = 'jokes.txt';
+        let path = __dirname + '/data/';
+        let filepath = path + filename;
+
+        return new Promise((resolve, reject) => {
+            fs.readFile(filepath, function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let string = data.toString();
+                    let lines = string.split('\n');
+
+                    resolve(lines[Math.floor(Math.random() * lines.length)]);
+                    fs.close();
+                }
+            });
+        });
+    }
 };
