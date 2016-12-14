d8b4d5471dc20ed5a3bd63c8da4cce980cb01d44
Evan Bechtol <evanbechtol@gmail.com>
2016-12-14 08:39:36 -0600

    refactored joke generation updated logging feature added comments to methods

--- client/index.js
+++ client/index.js
@@ -2,16 +2,23 @@ let fs = require('fs');
 const Promise = require('es6-promise').Promise;
 
 module.exports = (client, Events, _) => {
+    /*
+     Once the client is connected to the gateway, it will connect
+     to the guilds that it has been assigned to.
+     */
     client.Dispatcher.on(Events.GATEWAY_READY, () => {
         console.log('Connected as: ' + client.User.username);
         try {
-            let guilds = client.Guilds.toArray();
-            let guild_names = [];
+            let guilds = client.Guilds.toArray(),
+                guild_names = [],
+                now = new Date();
+
             for (let i = 0; i < guilds.length; i++) {
                 guild_names.push(guilds[i].name);
             }
-            let now = new Date();
+
             let output = '[' + now + '] Connected to guilds: ' + JSON.stringify(guild_names);
+
             writeToLog(output)
                 .then(() => {
                     console.log(output);
@@ -19,12 +26,17 @@ module.exports = (client, Events, _) => {
                 .catch((e) => {
                     console.log(JSON.stringify(e));
                 });
+
         } catch (e) {
             console.log(e);
         }
 
     });
 
+    /*
+     Keeps track of users entering any voice channel, on the guilds
+     that the bot is connected to
+     */
     client.Dispatcher.on(Events.VOICE_CHANNEL_JOIN, (e) => {
         data = {
             username: e.user.username,
@@ -45,11 +57,20 @@ module.exports = (client, Events, _) => {
             });
     });
 
+    /*
+     Watches for the '!joke' command in all channels, in the guilds
+     that the bot is connected to. Once the command is received, the
+     bot retrieves a random joke line from the file, and returns it to
+     the user that has been mentioned in the message.
+     */
     client.Dispatcher.on(Events.MESSAGE_CREATE, (e) => {
         //console.log(e.message);
         generateJoke(e);
     });
 
+    /*
+     Helper method to retrieve random lines from the joke file
+     */
     function getRandomLine() {
         let filename = 'jokes.txt';
         let path = __dirname + '/data/';
@@ -69,42 +90,49 @@ module.exports = (client, Events, _) => {
         });
     }
 
+    /*
+     Generates the joke and message. Checks to ensure proper message
+     format is given.
+     */
     function generateJoke(e) {
         let data = e.message.content.split(' ');
-        try {
-            if (_.first(data) === '!joke' && data.length > 1) {
-                if (e.message.mentions && e.message.mentions.length > 0) {
-                    let name = e.message.mentions[0].username,
-                        now = new Date(),
-                        message = "";
+        if (_.first(data) === '!joke') {
+            if (e.message.mentions && e.message.mentions.length > 0) {
+                let name = e.message.mentions[0].username,
+                    now = new Date(),
+                    message = "";
 
-                    getRandomLine()
-                        .then((joke) => {
-                            message = name + joke;
-                            e.message.channel.sendMessage(message);
-                        })
-                        .then(() => {
-                            let output = '[' + now + '] Sender: \"' + e.message.author.username + '\" Receiver: \"' + name + '\"';
-                            writeToLog(output)
-                                .then(() => {
-                                    console.log(output);
-                                });
-                        });
-                } else {
-                    throw(mentionError);
-                }
-            } else if (e.message.content === '!joke' && e.message.mentions.length === 0) {
-                throw(mentionError);
+                getRandomLine()
+                    .then((joke) => {
+                        message = name + joke;
+                        e.message.channel.sendMessage(message);
+                    })
+                    .then(() => {
+                        let output = '[' + now + '] Sender: \"' + e.message.author.username + '\" Receiver: \"' + name + '\"';
+                        return writeToLog(output);
+                    })
+                    .then((result) => {
+                        console.log(result);
+                    })
+                    .catch((e) => {
+                        writeToLog(e)
+                            .then((result) => {
+                                console.log('Error writing to file: ' + result);
+                            });
+                    });
+            } else {
+                e.message.channel.sendMessage('You must mention a user using @<username>');
             }
-        } catch (mentionError) {
-            e.message.channel.sendMessage('You must mention a user using @<username>');
         }
     }
 
+    /*
+     Writes all events to log for later review
+     */
     function writeToLog(data) {
         return new Promise((resolve, reject) => {
             let path = __dirname + '/log.txt';
-            fs.appendFile(path, data + '\r\n', {flags: 'a+'}, (err, data) => {
+            fs.appendFile(path, data + '\r\n', (err, result) => {
                 if (err) {
                     return reject(err);
                 } else {
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let Discordie = require('discordie');
-let _ = require('underscore');
-const config = require('./config/');
-const Events = Discordie.Events;
-const client = new Discordie({autoReconnect: true});
+let Discordie = require('discordie'),
+    _ = require('underscore');
+
+const config = require('./config/'),
+    Events = Discordie.Events,
+    client = new Discordie({autoReconnect: true});
 
 require('./client/')(client, Events, _);
 
+/*
+ Attempt to connect to the discord guilds that the bot
+ is assigned to, or able to join.
+ */
 try {
     client.connect({
         token: config.token
